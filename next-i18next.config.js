497016a153b9365ea1b55f432eeb91989448dca4
Achref-Creo2022 <achref.creo.developer@gmail.com>
2022-06-03 10:03:56 +0200

    100%

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -4,7 +4,7 @@ module.exports = {
   i18n: {
     locales: ["en", "fr"],
     defaultLocale: "en",
-    localePath: path.resolve("./public/locales"),
+    // localePath: path.resolve("./public/locales"),
   },
   react: { useSuspense: false },
 };

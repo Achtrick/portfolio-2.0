3845d82dfbfa94e613690d2cf878853dcb259efa
Maamoun <grissa.maamoun@gmail.com>
2022-06-24 01:27:20 +0100

    google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,7 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import { useEffect, useState } from "react";
 import { AppContext } from "../components/AppContext";
+import Script from 'next/script';
 
 const clientSideEmotionCache = createCache({ key: "css" });
 
@@ -30,6 +31,19 @@ function MyApp({
   const [openItem, setOpenItem] = useState({});
 
   return (
+    <>
+    <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+
+    <Script strategy="lazyOnload" id="google-script">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+            page_path: window.location.pathname,
+            });
+        `}
+    </Script>
     <AppContext.Provider
       value={{ openView, setOpenView, openItem, setOpenItem }}
     >
@@ -43,6 +57,7 @@ function MyApp({
         </SnackbarProvider>
       </CacheProvider>
     </AppContext.Provider>
+    </>
   );
 }
 

e6c88340c971a6d41a2efd06cdd232d105f0f1eb
achtrick <achrefmtir7@gmail.com>
2023-04-24 00:56:48 +0100

    fix portfolio projects order

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -225,10 +225,10 @@ export default function Portfolio(props) {
               {projects
                 .sort((a, b) => {
                   if (a.order < b.order) {
-                    return -1;
+                    return 1;
                   }
                   if (a.order > b.order) {
-                    return 1;
+                    return -1;
                   }
                   return 0;
                 })
